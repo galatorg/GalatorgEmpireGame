719b2e781906b1e33bddfdc565ae2ea1d27be28b
galatorg <xonox@galatorg.com>
2013-12-07 19:35:04 -0500

    defaultTiles setup
    Added defaultFont.psd to go with the PNG.  Created a defaultTiles
    spritesheet(PSD and PNG) and changed the coding so that it could now be
    used.  This sheet is 16 tiles wide, whereas the old one was 32, so we
    know that different(on x) sized tile grids will work fine.  Now testing
    is just needed with different tile sizes.  SpriteSheet now requires the
    tileSize to be put into the constructor, the old constructor was pulled
    out.

--- src/org/galat/empiregame/gfx/SpriteSheet.java
+++ src/org/galat/empiregame/gfx/SpriteSheet.java
@@ -10,7 +10,8 @@ import javax.imageio.ImageIO;
  * SpriteSheet class                                                         *
  *                                                                           *
  * Processes a graphics image so that it can be used as a sprite sheet for   *
- * displaying tiles on the game area.                                        *
+ * displaying tiles on the game area.  Can do spritesheets that are 2x2,     *
+ * 4x4, 8x8, 16x16, 32x32, 64x64,...                                         *
  *                                                                           *
 \*****************************************************************************/
 
@@ -22,12 +23,12 @@ public class SpriteSheet
 	public int[] pixels; // holds the image data
 	
 	// predefined spritesheets
-	public static SpriteSheet defaultTiles = new SpriteSheet("/sprite_sheet.png");
-	public static SpriteSheet defaultPlayer = new SpriteSheet("/sprite_sheet.png");
-	public static SpriteSheet defaultFont = new SpriteSheet("/spritesheets/default/defaultFont.png");
+	public static SpriteSheet defaultTiles = new SpriteSheet("/spritesheets/default/defaultTiles.png", 32);
+	public static SpriteSheet defaultPlayer = new SpriteSheet("/sprite_sheet.png", 32);
+	public static SpriteSheet defaultFont = new SpriteSheet("/spritesheets/default/defaultFont.png", 32);
 	
 	// constructor for a basic 4 color spritesheet (you can replace the 4 colors with whatever color you want)
-	public SpriteSheet(String path)
+	public SpriteSheet(String path, int size)
 	{
 		BufferedImage image = null; // variable to read the image data into
 		
@@ -53,50 +54,11 @@ public class SpriteSheet
 		
 		// since the data read into the image variable, we'll store the rest of the details
 		this.path = path;
-		this.tileSize = 32; // default tile size of 32
-		this.bitsNeeded = 5;
+		this.tileSize = size;
 		this.width = image.getWidth();
 		this.height = image.getHeight();
 		this.tilesOnX = this.width / this.tileSize; // calculate the number of tiles across
-
-		pixels = image.getRGB(0, 0, width, height, null, 0, width); // turn the image into an int[] of 0xARGB
-		
-		for (int i = 0; i < pixels.length; i++) // process each pixel
-		{
-			pixels[i] = (pixels[i] & 0xff)/64; // cuts off the alpha channel and trims the blue channel down to 2 bits(4 colors in the spritesheet that can be used)
-		}
-	}
-	
-	public SpriteSheet(String path, int tileSize)
-	{
-		BufferedImage image = null; // variable to read the image data into
-		
-		try // attempt to read in the image data
-		{
-			image = ImageIO.read(SpriteSheet.class.getResourceAsStream(path)); // open the file and read the contents into image
-		}
-		catch (IOException e) // if there was a problem opening the file and reading it
-		{
-			e.printStackTrace();
-		}
-		
-		if (image == null) // if there is no data in the variable
-		{
-			this.path = "";
-			this.height = 0;
-			this.width = 0;
-			this.tileSize = 0;
-			this.bitsNeeded = 0;
-			this.tilesOnX = 0;
-			return; // do not continue
-		}
-		
-		// since the data read into the image variable, we'll store the rest of the details
-		this.path = path;
-		this.tileSize = tileSize;
-		this.width = image.getWidth();
-		this.height = image.getHeight();
-		this.tilesOnX = this.width / this.tileSize; // calculate the number of tiles across
+		System.out.println(tilesOnX);
 		
 		int j=0;
 		int i=1;
@@ -105,6 +67,13 @@ public class SpriteSheet
 			i *= 2; // increase by a power of 2
 			j++; // count 1 bit - 2^j = i
 		}
-		this.bitsNeeded = j; 		
+		this.bitsNeeded = j;
+
+		pixels = image.getRGB(0, 0, width, height, null, 0, width); // turn the image into an int[] of 0xARGB
+
+		for (i = 0; i < pixels.length; i++) // process each pixel
+		{
+			pixels[i] = (pixels[i] & 0xff)/64; // cuts off the alpha channel and trims the blue channel down to 2 bits(4 colors in the spritesheet that can be used)
+		}
 	}
 }
