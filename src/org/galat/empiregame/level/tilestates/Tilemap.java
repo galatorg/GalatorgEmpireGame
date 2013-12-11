1deae4a941c524c0a2e6200d678b0052312441ae
galatorg <xonox@galatorg.com>
2013-12-10 22:21:11 -0500

    Left/Right/Above/Below setup when level loaded
    Above, Below, Left, and Right references setup for the Tilestate class
    for the Tilemap while Tilemap is loading up the level.  Has not been
    tested, but should work for any size levels(:D)

--- src/org/galat/empiregame/level/tilestates/Tilemap.java
+++ src/org/galat/empiregame/level/tilestates/Tilemap.java
@@ -54,7 +54,7 @@ public class Tilemap
 		}		
 	}
 	
-	// loads the tileId's into the tiles array based on the data stored in image
+	// loads the tileId's into the tiles array based on the data stored in image and link the TileStates
 	private void loadTiles()
 	{
 		int[] tileColors = this.image.getRGB(0, 0, width, height, null, 0, width); // get the RGB of each pixel in the image and store it in an int[]
@@ -72,6 +72,70 @@ public class Tilemap
 				}
 			} // check the next column
 		} // check the next row
+		
+		// setup references on the top and bottom rows		
+		if (height > 1) //if there is only 1 row then above/below will still be null for all
+		{
+			for (int x = 0; x < width; x++) // for each tile along x
+			{
+				this.tiles[x][0].below = this.tiles[x][1]; // set the top row's below reference 
+				this.tiles[x][height-1].above = this.tiles[x][height-2]; // set the bottom row's above reference
+			}
+		}
+		
+		// setup references on the first(left) and last(right) columns
+		if (width > 1) // if there is only 1 column then left/right will still be null for all
+		{
+			for (int y = 0; y < height; y++) // for each tile along y
+			{
+				this.tiles[0][y].right = this.tiles[1][y]; // set the first(left) row's right reference
+				this.tiles[width-1][y].left = this.tiles[width-2][y]; // set the last(right) row's left reference
+			}
+		}
+		
+		// setup the references on the inner tiles
+		if (height > 2) // if there are inner tiles along the rows, at least 3 rows
+		{
+			for (int y = 1; y < height-1; y++) // for each row in the level, skip top and bottom rows
+			{
+				if (width > 2) // if there are inner tiles along the columns, at least 3 columns
+				{
+					for (int x = 1; x < width-1; x++) // for each column in the level, skip first and last columns
+					{
+						this.tiles[x][y].above = this.tiles[x][y-1];
+						this.tiles[x][y].below = this.tiles[x][y+1];
+						this.tiles[x][y].left = this.tiles[x-1][y];
+						this.tiles[x][y].right = this.tiles[x+1][y];
+					} // next column
+				}
+				else // there are inner tiles along the rows, but not columns, width = 1 or 2, don't need to set left/right
+				{
+					this.tiles[0][y].above = this.tiles[0][y-1];
+					this.tiles[0][y].below = this.tiles[0][y+1];
+					if (width==2)
+					{
+						this.tiles[1][y].above = this.tiles[1][y-1];
+						this.tiles[1][y].below = this.tiles[1][y+1];
+					}
+				}
+			} // next row
+		}
+		else // don't need to set the above/below because there are only 1-2 rows, height = 1 or 2
+		{
+			if (width > 2) // if there are inner tiles along the columns, at least 3 columns, width = 1 or 2
+			{
+				for (int x = 1; x < width-1; x++) // for each column in the level, skip first and last columns
+				{
+					this.tiles[x][0].left = this.tiles[x-1][0];
+					this.tiles[x][0].right = this.tiles[x+1][0];
+					if (height==2)
+					{
+						this.tiles[x][1].left = this.tiles[x-1][1];
+						this.tiles[x][1].right = this.tiles[x+1][1];
+					}
+				} // next column
+			}
+		}
 	}
 	
 	// temporarily empty until it's needed.  used to update the state of the blocks
